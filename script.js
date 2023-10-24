d7184778e810271ea63dd7e921756d022b3e21a4
Eltfvzh <148296570+Eltfvzh@users.noreply.github.com>
2023-10-24 21:50:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let nilai =prompt("masukan nilai nilai yang anda inginkan");
+console.log(nilai);
+
+let a = 20
+let b = 10
+
+var hasil = b + a;
+console.log(hasil);
+
+var hasil1 = b - a;
+console.log(hasil1);
+
+var hasil2 = b * a;
+console.log(hasil2);
+
+var hasil3 = b / a;
+console.log(hasil3);
+
+var hasil4 = b ** a;
+console.log(hasil4);
+
+var hasil5 = b % a;
+console.log(hasil5);
+
+var hasil6 = a - b;
+console.log(hasil6);
+
+var hasil7 = a * b;
+console.log(hasil7);
+
+var hasil8 = a / b;
+console.log(hasil8);
+
+var hasil9 = a ** b;
+console.log(hasil9);
+
+var hasil10 = a % b;
+console.log(hasil10);
+
+
+if(nilai == "10+20"){
+    document.write(`<h1>${hasil}</h1>`)
+}else if(nilai == "10-20"){
+    document.write(`<h1>${hasil1}</h1>`)
+}else if(nilai == "10*20"){
+    document.write(`<h1>${hasil2}</h1>`)
+}else if(nilai == "10/20"){
+    document.write(`<h1>${hasil3}</h1>`)
+}else if(nilai == "10**20"){
+    document.write(`<h1>${hasil4}</h1>`)
+}else if(nilai == "10%20"){
+    document.write(`<h1>${hasil5}</h1>`)
+}else if(nilai == "20+10"){
+    document.write(`<h1>${hasil}</h1>`)
+}else if(nilai == "20-10"){
+    document.write(`<h1>${hasil6}</h1>`)
+}else if(nilai == "20*10"){
+    document.write(`<h1>${hasil7}</h1>`)
+}else if(nilai == "20/10"){
+    document.write(`<h1>${hasil8}</h1>`)
+}else if(nilai == "20**10"){
+    document.write(`<h1>${hasil9}</h1>`)
+}else if(nilai == "20%10"){
+    document.write(`<h1>${hasil10}</h1>`)
+}
